3e4ded8b28d4e5f5c8543d23fdcdbb67e6d38f87
Christopher Lee <hanernlee@gmail.com>
2018-04-14 22:10:01 +1000

    Init Game

---
+++ ElonGame/GameScene.swift
@@ -0,0 +1,79 @@
+//
+//  GameScene.swift
+//  ElonGame
+//
+//  Created by Christopher Lee on 14/4/18.
+//  Copyright © 2018 Christopher Lee. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var player: SKNode?
+    var joystick : SKNode?
+    var joystickKnob: SKNode?
+    
+    var joystickAction = false
+    var knobRadius: CGFloat = 50.0
+    
+    override func didMove(to view: SKView) {
+        player = childNode(withName: "player")
+        joystick = childNode(withName: "joystick")
+        joystickKnob = childNode(withName: "knob")
+    }
+}
+
+// MARK: Touches
+extension GameScene {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            if let joystickKnob = joystickKnob {
+                let location = touch.location(in: joystick!)
+                joystickAction = joystickKnob.frame.contains(location)
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let joystick = joystick else { return }
+        guard let joystickKnob = joystickKnob else { return }
+        
+        if !joystickAction { return }
+        
+        for touch in touches {
+            let position = touch.location(in: joystick)
+            
+            let length = sqrt(pow(position.y, 2) + pow(position.x, 2))
+            let angle = atan2(position.y, position.x)
+            
+            if knobRadius > length {
+                joystickKnob.position = position
+            } else {
+                joystickKnob.position = CGPoint(x: cos(angle) * knobRadius, y: sin(angle) * knobRadius)
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let xJoystickCoordinate = touch.location(in: joystick!).x
+            let xLimit: CGFloat = 200.0
+            
+            if xJoystickCoordinate > -xLimit && xJoystickCoordinate < xLimit {
+                resetKnobPosition()
+            }
+        }
+    }
+}
+
+// MARK: Action
+extension GameScene {
+    func resetKnobPosition() {
+        let initialPoint = CGPoint(x: 0, y: 0)
+        let moveBack = SKAction.move(to: initialPoint, duration: 0.1)
+        moveBack.timingMode = .linear
+        joystickKnob?.run(moveBack)
+        joystickAction = false
+    }
+}
